6e1c5581bfb841efc15a4726e9952310f656e338
Aleksandr Chernov <sashacool75@gmail.com>
2025-07-12 22:06:14 +0300

    change origin

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Accept",
+  });
+
   const config = new DocumentBuilder()
-    .setTitle('Country')
-    .setDescription('The country API description')
-    .setVersion('1.0')
-    .addTag('country')
+    .setTitle("Country")
+    .setDescription("The country API description")
+    .setVersion("1.0")
+    .addTag("country")
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup("api", app, documentFactory);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,11 +27,6 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    credentials: true,
-  });
   await app.listen(process.env.PORT ?? 5005);
 }
 
